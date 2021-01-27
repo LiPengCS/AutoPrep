c3437155b0da63beefac0a253e1cd9e1ebe26564
Peng Li <lipengpublic@gmail.com>
2021-01-26 19:57:12 -0500

    update

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+python mv_main2.py --data_dir <数据文件夹路径> --dataset <数据集名称> --result_dir <结果文件夹路径> 
+
+<数据文件夹路径>: 默认 data/realmv_datasets_pmnn
+<数据集名称>: 默认 None, 运行文件夹内所有数据集
+<结果文件夹路径> : 默认 result/
--- mv_main2.py
+++ mv_main2.py
@@ -9,15 +9,23 @@ from model import LogisticRegression
 import argparse
 import os
 
-data_dir = "data/realmv_datasets_pmnn"
-result_dir = "result0122_2"
-datasets = [d for d in os.listdir(data_dir) if d[0] != "."]
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default=None)
+parser.add_argument('--data_dir', default="data/realmv_datasets_pmnn")
+parser.add_argument('--result_dir', default="result")
+args = parser.parse_args()
+
+if args.dataset is not None:
+    datasets = [args.dataset]
+else:
+    # run all datasets if dataset is not specified
+    datasets = [d for d in os.listdir(args.data_dir) if d[0] != "."]
 
 for dataset in datasets:
     print(dataset)
 
     # load data
-    X, y = utils.load_data(data_dir, dataset, keepna=True)
+    X, y = utils.load_data(args.data_dir, dataset, keepna=True)
 
     # split data to train/val/test and preprocess data
     X_train_mv, y_train, X_val_mv, y_val, X_test_mv, y_test = build_data(X, y)
@@ -45,7 +53,7 @@ for dataset in datasets:
     # initialize imputer
     mv_imputer = MVGridImputeLayer(X_train_mv, placeholder, n_grid=5)
     model = LogisticRegression(input_dim, output_dim).to(device=params["device"])
-    save_dir = utils.makedir([result_dir, dataset, "imputation"])
+    save_dir = utils.makedir([args.result_dir, dataset, "imputation"])
     run_experiment(model, mv_imputer, params, X_train, y_train, X_val, y_val, X_test, y_test, save_dir)
 
     ## baseline: mean imputation
@@ -53,7 +61,7 @@ for dataset in datasets:
     X_train_default = mean_imputer.fit_transform(X_train_mv)
     X_val_default = mean_imputer.transform(X_val_mv)
     X_test_default = mean_imputer.transform(X_test_mv)
-    save_dir = utils.makedir([result_dir, dataset, "Mean"])
+    save_dir = utils.makedir([args.result_dir, dataset, "Mean"])
     model = LogisticRegression(input_dim, output_dim).to(device=params["device"])
     transformer = None
     run_experiment(model, transformer, params, X_train_default, y_train, X_val_default, y_val, X_test_default, y_test, save_dir)
